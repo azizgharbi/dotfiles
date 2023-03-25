3bd49a2af65ceeb0d33e231004c160c82e286a7c
Aziz <med.aziz.gharbii@gmail.com>
2023-03-25 18:30:43 -0400

    feat: Updates and add keymaps

--- nvim.bak/lua/src/keymaps.lua
+++ nvim.bak/lua/src/keymaps.lua
@@ -1,6 +1,4 @@
 local opts = { noremap = true, silent = true }
-local bufopts = { noremap = true, silent = true, buffer = bufnr }
-local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
