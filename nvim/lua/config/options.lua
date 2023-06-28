2ac46fd85f22d56e381265ce7208a767446c6373
Aziz <med.aziz.gharbii@gmail.com>
2023-06-27 23:22:06 -0400

    feat: update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local error_file_path = "/var/log/nvim.log"
+if error_file_path then
+  vim.opt.errorfile = error_file_path
+end
